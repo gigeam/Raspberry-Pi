320322ac276cc9cb656031979d3b7e2f8a7fd5f6
Mihaela Gigea <gigeam@uni.coventry.ac.uk>
2017-11-11 00:38:00 +0000

    added orientation code

--- test_sense_hat.py
+++ test_sense_hat.py
@@ -2,8 +2,17 @@ from sense_hat import SenseHat
 
 
 sense = SenseHat()
+
+# print stuff to LED
+"""
 blue = (0, 0, 255)
 yellow = (255, 255, 0)
-
 while True:
   sense.show_message("Astro Pi is awesome!", text_colour=yellow, back_colour=blue, scroll_speed=0.05)
+"""
+
+# show pitch, roll, yaw
+while True:
+    orientation = sense.get_orientation()
+    print("Pitch %d, Roll %d, Yaw %d" % (orientation["pitch"], orientation["roll"], orientation["yaw"]))
+    
