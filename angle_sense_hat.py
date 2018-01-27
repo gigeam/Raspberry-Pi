3f01bd4038a195a3aebaf6bbe4a7c44d4f670330
Mihaela Gigea <gigeam@uni.coventry.uk>
2018-01-27 23:58:34 +0000

    added ability to change file

--- angle_sense_hat.py
+++ angle_sense_hat.py
@@ -1,4 +1,6 @@
 from sense_hat import SenseHat
+import keyboard
+import pandas as pd
 
 
 sense = SenseHat()
@@ -11,7 +13,26 @@ while True:
   sense.show_message("Astro Pi is awesome!", text_colour=yellow, back_colour=blue, scroll_speed=0.05)
 """
 
+angles_list = []
 # show pitch, roll, yaw, using accelerometer, gyroscope and magnetometer (for best accuracy)
 while True:
     orientation = sense.get_orientation()
-    print("Pitch %d, Roll %d, Yaw %d" % (orientation["pitch"], orientation["roll"], orientation["yaw"]))
+    angle = {"pitch": orientation["pitch"], "roll": orientation["roll"], "yaw": orientation["yaw"]}
+    if keyboard.is_pressed("f"):
+        angle["command"] = "forward"
+    elif keyboard.is_pressed("b"):
+        angle["command"] = "backward"
+    elif keyboard.is_pressed("l"):
+        angle["command"] = "left"
+    elif keyboard.is_pressed("r"):
+        angle["command"] = "right"
+    else:
+        angle["command"] = "stop"
+        
+    print("Pitch %d, Roll %d, Yaw %d, Command %s" % (angle["pitch"], angle["roll"], angle["yaw"], angle["command"]))    
+    angles_list.append(angle)
+    if keyboard.is_pressed("q"):
+        print("Saving angle dataframe..")
+        pd.DataFrame(angles_list).to_csv("angles.csv", index=False)
+        print("Exiting program...")
+        break
