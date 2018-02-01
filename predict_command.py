1b5717a295c2a3e5459d92984fa70a93fbb5595a
Mihaela Gigea <gigeam@uni.coventry.ac.uk>
2018-02-01 23:18:04 +0000

    undo the angle debugging

--- predict_command.py
+++ predict_command.py
@@ -13,7 +13,6 @@ while True:
     orientation = sense.get_orientation()
     # extract the two dimensions i.e. pitch and roll
     angle = [orientation["pitch"], orientation["roll"]]
-    angle = [360, 360]
     predict_command(clf, angle)
     # exit the program
     if keyboard.is_pressed("q"):
