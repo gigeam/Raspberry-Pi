5045331b7f53bb246433c6bff6615d900b239d55
Mihaela Gigea <gigeam@uni.coventry.ac.uk>
2018-02-04 00:26:13 +0000

    added the import back

--- train_classifier.py
+++ train_classifier.py
@@ -1,5 +1,5 @@
 import numpy as np
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 from sklearn import neighbors
 from sklearn.externals import joblib
