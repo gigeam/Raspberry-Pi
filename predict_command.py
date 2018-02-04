74cd63f1a6fee31289a749ae153d89600ddb4234
Mihaela Gigea <gigeam@uni.coventry.ac.uk>
2018-02-04 12:49:10 +0000

    using uniform knn

--- predict_command.py
+++ predict_command.py
@@ -5,7 +5,7 @@ from knn_predict import predict_command
 # instantiate sense hat object
 sense = SenseHat()
 # load pre-trained classifier from file
-clf = joblib.load('knn_distance.pkl')
+clf = joblib.load('knn_uniform.pkl')
 # repeat forever, execution can be stopped using ctrl+c
 while True:
     # pitch, roll, yaw, using accelerometer, gyroscope and magnetometer (for best accuracy)
