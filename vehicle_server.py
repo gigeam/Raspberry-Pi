4a9242f15cd665f45e294b79ead7aa05b9eefd0b
Mihaela Gigea <gigeam@uni.coventry.ac.uk>
2018-02-25 12:08:16 +0000

    try except for vehicle server code

--- vehicle_server.py
+++ vehicle_server.py
@@ -42,8 +42,11 @@ if __name__ == "__main__":
     while True:
         # command_dictionary = {"val1": val_1, "val2": val_2, "val3": val_3}
         # server_socket.send_dict(connect, command_dictionary)
-        values_received = server_socket.get_dict(connect, 1024)
-        print(values_received)
+        try:
+            values_received = server_socket.get_dict(connect, 1024)
+            print(values_received)
+        except:
+            pass
     # close the socket
     server_socket.close()
 
