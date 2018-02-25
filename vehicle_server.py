f5f635654fb2e915678083b2f2f04bc2a22af627
Mihaela Gigea <gigeam@uni.coventry.ac.uk>
2018-02-25 19:06:06 +0000

    new approach

--- vehicle_server.py
+++ vehicle_server.py
@@ -11,33 +11,26 @@ class ServerSocket(object):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.server_socket.bind((self.host, self.port))
 
-    # accept connection
-    def accept(self):
-        return self.server_socket.accept()
-
     # close connection
     def close(self):
         self.server_socket.close()
 
     # receive dictionary
-    @staticmethod
-    def get_dict(connect, size):
-        dict_msg = connect.recvfrom(size)
-        return pickle.loads(dict_msg)
+    def get_dict(self, size):
+        data, address = self.server_socket.recvfrom(size)
+        return pickle.loads(data), address
 
 
 if __name__ == "__main__":
     # create a socket object
     server_socket = ServerSocket()
     print("listening to socket")
-    connection, address = server_socket.accept()
-    print("got connection from", address)
     while True:
         print(sys.stderr, '\nwaiting to receive message')
-        data = server_socket.get_dict(connection, 4096)
+        received_data, sender_address = server_socket.get_dict(4096)
 
-        print(sys.stderr, 'received %s bytes from %s' % (len(data), address))
-        print(sys.stderr, data)
+        print(sys.stderr, 'received %s bytes from %s' % (len(received_data), sender_address))
+        print(sys.stderr, received_data)
         """
         if data:
             sent = sock.sendto(data, address)
