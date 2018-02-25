d1595cf222ebf75e4b4f33370739f796cefe0bf6
Mihaela Gigea <gigeam@uni.coventry.ac.uk>
2018-02-25 13:16:57 +0000

    tweaking the server code

--- vehicle_server.py
+++ vehicle_server.py
@@ -43,12 +43,9 @@ if __name__ == "__main__":
     while True:
         # command_dictionary = {"val1": val_1, "val2": val_2, "val3": val_3}
         # server_socket.send_dict(connect, command_dictionary)
-        try:
-            values_received = server_socket.get_dict(connect, 1024)
-            print(values_received, count)
-            count += 1
-        except:
-            pass
+        values_received = server_socket.get_dict(connect, 1024)
+        print(values_received, count)
+        count += 1
     # close the socket
     server_socket.close()
 
