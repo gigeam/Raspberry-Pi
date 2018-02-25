1ffc543b8c31a99398c1bf682ebc1ea0d303ea61
Mihaela Gigea <gigeam@uni.coventry.ac.uk>
2018-02-25 12:39:04 +0000

    added counter to see if values are received

--- vehicle_server.py
+++ vehicle_server.py
@@ -39,12 +39,14 @@ if __name__ == "__main__":
     connect, address = server_socket.accept()
     print("got connection from", address)
     # val_1, val_2, val_3 = 1, 2, 3
+    count = 1
     while True:
         # command_dictionary = {"val1": val_1, "val2": val_2, "val3": val_3}
         # server_socket.send_dict(connect, command_dictionary)
         try:
             values_received = server_socket.get_dict(connect, 1024)
-            print(values_received)
+            print(values_received, count)
+            count += 1
         except:
             pass
     # close the socket
