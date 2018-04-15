b55f1ed9d561920deb510786cdac22d1168fbb92
Mihaela Gigea <gigeam@uni.coventry.ac.uk>
2018-04-15 20:48:17 +0100

    removed unused code and added comment

--- test_sense_hat.py
+++ test_sense_hat.py
@@ -1,15 +1,7 @@
 from sense_hat import SenseHat
 
-
+# instantiate a SenseHat object
 sense = SenseHat()
-
-# print stuff to LED
-"""
-blue = (0, 0, 255)
-yellow = (255, 255, 0)
-while True:
-  sense.show_message("Astro Pi is awesome!", text_colour=yellow, back_colour=blue, scroll_speed=0.05)
-"""
 
 # show pitch, roll, yaw, using accelerometer, gyroscope and magnetometer (for best accuracy)
 while True:
