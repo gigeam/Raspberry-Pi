4ad92b579c8fe2a1ced95e7e844926611978c77e
Mihaela Gigea <gigeam@uni.coventry.ac.uk>
2018-04-22 19:24:56 +0100

    renamed unused imports

--- stepper_test_custom_single.py
+++ stepper_test_custom_single.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_Stepper
-from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_StepperMotor
+from Adafruit_MotorHAT import Adafruit_MotorHAT
 
 import time
 import atexit
